017b1a30103a7286fce682add94494d8a3442c25
caio-santos-ios <caiosantos.dev@outlook.com>
2024-03-24 21:08:36 -0300

    initial commit

---
+++ src/main/java/br/com/app_docker/app_docker/Controller.java
@@ -0,0 +1,15 @@
+package br.com.app_docker.app_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    
+    @GetMapping("/helloWord")
+    public String read() {
+        return "Hello word.";
+    }
+}
---
+++ src/test/java/br/com/app_docker/app_docker/AppDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app_docker.app_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
